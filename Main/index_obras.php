e3d61500d36b4387455c913b2bc452ba00588791
GinOnline <est.spadoni.gino@latecnicalf.com.ar>
2022-09-24 16:55:05 -0300

    Testing

--- Main/index_obras.php
+++ Main/index_obras.php
@@ -63,16 +63,16 @@
                       <a class = "header-btn" href="historial.php">Historial</a>
                   </li>
                   <li>
-                    <button type="button" class= "header-btn" data-toggle="modal" data-target="#exampleModalLong">
+                    <button type="button" class="header-btn" data-toggle="modal" data-target=".bd-example-modal-lg">
                       Mapa
-                    </button>          
+                    </button>     
                   </li>
-                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
 
                   <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg">
                       <div class="modal-content">
-                        ...
+                        <img src="./img/plano_gral.png" alt="Plano General">
                       </div>
                     </div>
                   </div>
