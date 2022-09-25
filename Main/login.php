e46905bbecd95d42773dbada6b3d33b71aa09f98
GabrielQ010 <est.quiroga.gabriel@latecnicalf.com.ar>
2022-09-24 21:59:05 -0300

    cambios estéticos finales - login - footer (GG)

--- Main/login.php
+++ Main/login.php
@@ -54,6 +54,10 @@ if (!empty($_POST['txtusuario']) && !empty($_POST['txtpassword'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&display=swap" rel="stylesheet">
     
    
 </head>
